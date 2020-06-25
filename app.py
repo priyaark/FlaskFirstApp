12fbc728ac4eaf814ba157e4d976d477e27c1320
Pri <priya.ark@gmail.com>
2020-06-25 12:24:55 -0400

    git commit -m "Format"

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
